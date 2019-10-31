99da0ec69c079e2dd53f938702f7eef4ffe2baa5
Andrey Kondalov <i@yukio.ru>
2019-10-31 13:34:29 +0400

    Places ctiy declensions & translate

--- src/actions/places.js
+++ src/actions/places.js
@@ -1,11 +1,29 @@
 import qs from 'query-string';
+import Morphy from 'phpmorphy';
+import translate from '@k3rn31p4nic/google-translate-api';
 import get from 'lodash/get';
 import sample from 'lodash/sample';
+import lowerCase from 'lodash/lowerCase';
+import upperCase from 'lodash/upperCase';
 import Action from './Action';
 
 const { FSQR_CLIENT_ID, FSQR_CLIENT_SECRET } = process.env;
 const HOST = 'https://api.foursquare.com/v2';
 const ERR_MSG = 'Извини, я забыл как искать. Возможно когда-нибудь вспомню.';
+
+const userCitiesEnum = {
+  'Лимасе': 'Лимассол',
+  'Лимас': 'Лимассол',
+  'Нижний': 'Нижний Новгород',
+  'Нижнем': 'Нижний Новгород',
+  'спб': 'Санкт-Петербург',
+  'мск': 'Москва',
+  'тлт': 'Тольятти',
+};
+
+const restrictedCities = [
+  'Солт Лейк Сити',
+];
 
 const phrases = [
   'сходить в',
@@ -14,11 +32,11 @@ const phrases = [
   'найти в',
   'собраться в',
   'поесть в',
+  'еда в',
 ];
 
 const section = sample(['food', 'drinks', 'coffee']);
-
-const rx = new RegExp(`(${phrases.join('|')}) ([A-Za-zА-Яа-я0-9]+)`, 'i');
+const rx = new RegExp(`(${phrases.join('|')}) ([A-Za-zА-Яа-я0-9- ]+)`, 'i');
 
 export default class PlacesAction extends Action {
   test(message) {
@@ -27,7 +45,7 @@ export default class PlacesAction extends Action {
 
   async getPhoto(v, venueId) {
     if (!FSQR_CLIENT_ID && !FSQR_CLIENT_SECRET) {
-      return ERR_MSG;
+      return null;
     }
     try {
       const res = await fetch(
@@ -57,7 +75,9 @@ export default class PlacesAction extends Action {
 
   async findVenues(v, near) {
     if (!FSQR_CLIENT_ID && !FSQR_CLIENT_SECRET) {
-      return ERR_MSG;
+      return {
+        caption: ERR_MSG,
+      };
     }
     try {
       const res = await fetch(
@@ -87,8 +107,14 @@ export default class PlacesAction extends Action {
       const promises = items.map(({ venue }) => this.getPhoto(v, venue.id));
       const prePhotos = await Promise.all(promises);
       const photos = prePhotos.filter(e => !!e);
+      const cityAddress = get(res, 'response.geocode.displayString');
+      if (!cityAddress) {
+        return {
+          caption: 'Извини, я не нашел ничего в этом городе.',
+        }
+      }
       const caption = `
-В <b>${get(res, 'response.geocode.displayString')}</b>, есть несколько мест, которые сейчас открыты:
+В <b>${cityAddress}</b>, есть несколько мест, которые сейчас открыты:
 ${venues.map((venue, index) => `
 ${index + 1}. <i>${venue.name}</i>${!!venue.address ? `
 ${venue.address}` : ''}
@@ -104,24 +130,56 @@ ${venue.address}` : ''}
     }
   }
 
+  async prepareCityName(_name) {
+    let name = lowerCase(_name);
+    if (restrictedCities.includes(name)) return name;
+    if (userCitiesEnum[name]) name = userCitiesEnum[name];
+
+    const morphy = new Morphy('ru', {
+      storage: Morphy.STORAGE_MEM,
+      predict_by_suffix: true,
+      predict_by_db: true,
+      graminfo_as_text: true,
+      use_ancodes_cache: false,
+      resolve_ancodes: Morphy.RESOLVE_ANCODES_AS_TEXT,
+    });
+
+    const cityPhrases = name.split(' ');
+    const res = morphy.lemmatize(cityPhrases);
+    const compCity = cityPhrases.map((e) => {
+      const val = res[upperCase(e)];
+      return val[val.length - 1];
+    });
+    const isError = compCity.some(e => !e);
+    let newCity;
+    if (isError) newCity = name;
+    else newCity = compCity.join(' ');
+    const { text }  = await translate(newCity, { to: 'en' });
+    return text;
+  }
+
   async doAction(message) {
     const { text } = message;
-    const [, , city] = rx.exec(text);
+    const [, , cityRaw] = rx.exec(text);
     const bot = this.bot;
     const chatId = message.chat.id || message.from.id;
     const options = { reply_to_message_id: message.message_id };
     bot.sendMessage(chatId, 'Уже бегу искать, оставайтесь на линии!', options);
+    const city = await this.prepareCityName(cityRaw);
     const { photos, caption } = await this.findVenues('20191029', city);
-    const media = photos.map((photo, index) => ({
-      type: 'photo',
-      media: photo,
-      ...(index === 0 ? {
-        caption,
-        parse_mode: 'html',
-      } : {}),
-      width: 500,
-      height: 500,
-    }));
+    let media = [];
+    if (photos && Array.isArray(photos)) {
+      media = photos.map((photo, index) => ({
+        type: 'photo',
+        media: photo,
+        ...(index === 0 ? {
+          caption,
+          parse_mode: 'html',
+        } : {}),
+        width: 500,
+        height: 500,
+      }));
+    }
     
     if (media.length) {
       bot.sendMediaGroup(chatId, media, options);
